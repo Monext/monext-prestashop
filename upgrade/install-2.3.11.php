7e0a6ce0ef7d99b546bf49a7940518dd7482f852
Dorian Garde <dorian.garde@tbdgroup.com>
2025-10-17 14:38:49 +0200

    Update Monext state attributes

---
+++ upgrade/install-2.3.11.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Payline module for PrestaShop
+ *
+ * @author    Monext <support@payline.com>
+ * @copyright Monext - http://www.payline.com
+ */
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+function upgrade_module_2_3_11(payline $module)
+{
+    $module->createCustomOrderState();
+    return true;
+}
